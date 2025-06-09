2418be14a1e00fa186284cf927d7474b333ef558
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-06-09 20:35:18 +0300

    Implementing GlobalExceptionHandler to render proper bad request messages

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -25,6 +25,8 @@ public class PatientMapper {
         patient.setEmail(patientRequestDTO.getEmail());
         patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
         patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+
+        return patient;
     }
 
 }
