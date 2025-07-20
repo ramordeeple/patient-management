bb748ee4d9771e0d9121c5cab34e9096fdd9a27d
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-07-20 19:45:43 +0300

    wrote the sh script that finally werks

--- infrastructure/localstack-deploy.sh
+++ infrastructure/localstack-deploy.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+set -e
 
-set -e # Stops whenever any command fails
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_DEFAULT_REGION=us-east-1
 
-aws --endpoint-url=http://localhost:4566 cloudformation deploy \
-    --stack-name patient-management \
-    --template-file "./cdk.out/localstack.template.json"
+ENDPOINT=http://localhost:4566
+BUCKET=localstack-deployments
 
-aws --endpoint-url=http://localhost:4566 elbv2 describe-load-balancers \
-    --query "LoadBalancers[0].DNSName" --output text
+# 1. Create bucket (ignore errors if it exists)
+aws --endpoint-url=$ENDPOINT s3 mb s3://$BUCKET || true
+
+# 2. Upload template to S3
+aws --endpoint-url=$ENDPOINT s3 cp ./cdk.out/localstack.template.json s3://$BUCKET/template.json
+
+# 3. Deploy via create-stack
+aws --endpoint-url=$ENDPOINT cloudformation create-stack \
+  --stack-name patient-management \
+  --template-url http://localstack:4566/$BUCKET/template.json \
+  --capabilities CAPABILITY_NAMED_IAM
