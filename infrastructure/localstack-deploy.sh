3aba45cec1baa97de77c99c87deb790095943062
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-08-07 12:22:30 +0300

    making the sh script useful

--- infrastructure/localstack-deploy.sh
+++ infrastructure/localstack-deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -e
 
-export AWS_ACCESS_KEY_ID=test
-export AWS_SECRET_ACCESS_KEY=test
-export AWS_DEFAULT_REGION=us-east-1
+#export AWS_ACCESS_KEY_ID=test
+#export AWS_SECRET_ACCESS_KEY=test
+#export AWS_DEFAULT_REGION=us-east-1
 
 ENDPOINT=http://localhost:4566
 BUCKET=localstack-deployments
 
 # 1. Create bucket (ignore errors if it exists)
-aws --endpoint-url=$ENDPOINT s3 mb s3://$BUCKET || true
+#aws --endpoint-url=$ENDPOINT s3 cp ./cdk.out/localstack.template.json s3://$BUCKET/template.json
 
-# 2. Upload template to S3
-aws --endpoint-url=$ENDPOINT s3 cp ./cdk.out/localstack.template.json s3://$BUCKET/template.json
 
-# 3. Deploy via create-stack
-aws --endpoint-url=$ENDPOINT cloudformation create-stack \
+aws --endpoint-url=$ENDPOINT cloudformation deploy \
   --stack-name patient-management \
-  --template-url http://localstack:4566/$BUCKET/template.json \
+  --template-file "./cdk.out/localstack.template.json" \
   --capabilities CAPABILITY_NAMED_IAM
+
+aws --endpoint-url=$ENDPOINT elbv2 describe-load-balance \
+    --query "LoadBalancers[0].DNSName" --output text
